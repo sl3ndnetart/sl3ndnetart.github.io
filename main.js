577e2f2c685cc63db553fc5afdfadc76ebdc8fa0
sl3ndnetart <159007502+sl3ndnetart@users.noreply.github.com>
2024-02-05 11:58:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//VARIABLES GLOBALES
+
+//la page HTML a fini de charger, on peut donc
+//interagir avec via des scripts sans risque d'erreur
+document.addEventListener("DOMContentLoaded", function() {
+console.log('dom ready')
+})
