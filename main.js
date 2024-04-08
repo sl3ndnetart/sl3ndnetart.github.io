37d84dc82a6baeb981a50f8c57c05ba33b552b8a
sl3ndnetart <159007502+sl3ndnetart@users.noreply.github.com>
2024-04-08 09:18:21 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-//VARIABLES GLOBALES
-
-//la page HTML a fini de charger, on peut donc
-//interagir avec via des scripts sans risque d'erreur
-document.addEventListener("DOMContentLoaded", function() {
-console.log('dom ready')
-})
